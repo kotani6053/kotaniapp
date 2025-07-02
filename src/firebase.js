c1fc4e61d6adb40827c8168de15c30ec7de0fd6d
kotani6053 <yoshioka@e-kotani.co.jp>
2025-07-02 12:53:33 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
- apiKey: "AIzaSyBtLFNwnAqBYFR7eFi-bKaT4Vq9Zu15a6I",
-  authDomain: "kotaniapp-4f017.firebaseapp.com",
-  projectId: "kotaniapp-4f017",
-  storageBucket: "kotaniapp-4f017.firebasestorage.app",
+  apiKey: "AIzaSyAxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "your-project-id.firebaseapp.com",
+  projectId: "your-project-id",
+  storageBucket: "your-project-id.appspot.com",
   messagingSenderId: "623409374889",
-  appId: "1:623409374889:web:8aba02fed565f56c23abb8"};// Initialize Firebaseconst app = initializeApp(firebaseConfig
+  appId: "1:623409374889:web:8aba02fed565f56c23abb8"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const db = getFirestore(app);
+
+export { db };
