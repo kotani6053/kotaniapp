4a1a5ad48b519ca47353939aa0b14125b4bda848
kotani6053 <yoshioka@e-kotani.co.jp>
2025-07-04 09:35:07 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,21 @@
+// firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Firebase設定
 const firebaseConfig = {
   apiKey: "AIzaSyBtLFNwnAqBYFR7eFi-bKaT4Vq9Zu15a6I",
   authDomain: "kotaniapp-4f017.firebaseapp.com",
   projectId: "kotaniapp-4f017",
-  storageBucket: "kotaniapp-4f017.appspot.com", // ✅ 修正ここ！
+  storageBucket: "kotaniapp-4f017.appspot.com", // ← ".app" ではなく ".com"
   messagingSenderId: "623409374889",
   appId: "1:623409374889:web:ce7f3636fccb126323abb8"
 };
 
+// Firebase初期化
 const app = initializeApp(firebaseConfig);
+
+// Firestoreを取得
 const db = getFirestore(app);
 
 export { db };
