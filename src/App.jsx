94a2ff4ca03bae3c467240e3f4918445033bc42d
kotani6053 <yoshioka@e-kotani.co.jp>
2025-12-22 14:07:54 +0900

    Update App component layout and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,42 @@
 export default function App() {
   return (
-    <div style={{ fontSize: "48px", padding: "40px" }}>
-      映っていますか？
+    <div style={{ padding: "40px", fontFamily: "sans-serif" }}>
+      <h1 style={{ fontSize: "64px" }}>会議室予約</h1>
+
       <input
+        placeholder="名前"
         style={{
           width: "100%",
           height: "120px",
           fontSize: "48px",
+          marginTop: "20px",
         }}
       />
+
+      <select
+        style={{
+          width: "100%",
+          height: "120px",
+          fontSize: "48px",
+          marginTop: "20px",
+        }}
+      >
+        <option>1階食堂</option>
+        <option>2階会議室①</option>
+        <option>2階会議室②</option>
+        <option>3階応接室</option>
+      </select>
+
+      <button
+        style={{
+          width: "100%",
+          height: "140px",
+          fontSize: "56px",
+          marginTop: "30px",
+        }}
+      >
+        予約する
+      </button>
     </div>
   );
 }
