ad8a9acfb0a30e5bec65880c2d5b728200fe9e9f
kotani6053 <yoshioka@e-kotani.co.jp>
2025-12-22 14:03:35 +0900

    Wrap App component in React.StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,10 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
-createRoot(document.getElementById("root")).render(<App />);
+const container = document.getElementById("root");
+
+createRoot(container).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
